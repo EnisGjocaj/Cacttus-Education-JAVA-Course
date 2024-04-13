34377f3153442977e0be5eee86d40ca7d3b3ea31
EnisGjocaj <enisg1489@gmsil.com>
2024-04-13 14:46:31 +0200

    M

---
+++ src/Dita4.java
@@ -0,0 +1,35 @@
+public class Dita4 {
+    public static void main(String[] args){
+
+        //Operatoret prefix  ++ --
+
+        int number = 10;
+        number++;
+
+        System.out.println(number);
+
+        //System.out.print ta kthen rezultatin ne nje rresht e System.out.println ne nje rresht te ri
+
+
+
+
+        //Loops
+
+        //While loop
+
+        int x=1;
+        while(x <= 5){
+            System.out.println("x="+x);
+            x++; 
+        }
+
+        //Do while loop
+           
+        int y=10;
+        do{
+            System.out.println("y+"+y);
+            y++;
+        }
+        while(y < 5);
+    }
+}
